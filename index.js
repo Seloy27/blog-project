4773226dec980c518fb3d343a4a1155441f89efb
brennan <markbrennanlicon39@gmail.com>
2025-05-18 16:43:43 +0800

    hotfix

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 const darkmode = document.getElementById('darkmode');
 const main_content = document.getElementById('main-content');
 
-darkmode.addEventListener('click', event => {
+darkmode.addEventListener('mousedown', event => {
     main_content.classList.toggle('darkmode');
 });
